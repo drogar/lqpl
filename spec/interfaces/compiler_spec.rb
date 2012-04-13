ffb5ef7cf6fd388e5940096584f6df375664f7a0
Brett G. Giles <brett.giles@ucalgary.ca>
2012-04-13 13:22:31 -0600

    Starting Compiler Server
    try IPV6 first, then IP4 address.

--- spec/interfaces/compiler_spec.rb
+++ spec/interfaces/compiler_spec.rb
@@ -1,41 +1,43 @@
 require 'spec/spec_helper'
 
 describe Compiler do
-  context "when the compiler process is not running" do
-    it "generates an error when created with defaults" do
-      expect {
-        Compiler.new
-      }.to raise_error(
-        CompilerProcessNotFound,
-        /no process found on port 7863/
-      )
-    end
-    it "generates an error when created with port > 1000" do
-      expect {
-        Compiler.new 3456
-        }.to raise_error(
-        CompilerProcessNotFound,
-        /no process found on port 3456/
-        )
-    end
-  end
-  context "when the compiler process is running" do
+  # context "when the compiler process is not running" do
+  #   it "generates an error when created with defaults" do
+  #     expect {
+  #       Compiler.new.connect
+  #     }.to raise_error(
+  #       CompilerProcessNotFound,
+  #       /no process found on port 7683/
+  #     )
+  #   end
+  #   it "generates an error when created with port > 1000" do
+  #     expect {
+  #       (Compiler.new 3456).connect
+  #       }.to raise_error(
+  #       CompilerProcessNotFound,
+  #       /no process found on port 3456/
+  #       )
+  #   end
+  # end
+  context ", when the compiler process is running," do
+
     context "creation of the Compiler interface" do
       context "creation options for the compiler interface" do
         it "allows passing a port number > 1000 on creation" do
           @cmp = Compiler.new 3456
           @cmp.port.should == 3456
         end
-        it "allows default creation with a port of 7863" do
+        it "allows default creation with a port of 7683" do
           @cmp = Compiler.new
-          @cmp.port.should == 7863
+          @cmp.port.should == 7683
         end
       end
-      context "once the compiler interface has been created" do
+      context "once the Compiler interface has been created" do
         before :each do
           @cmp = Compiler.new
-        end 
-        it "connects to the compiler process when created" do
+          @cmp.connect
+        end
+        it "connects to the lqpl-compiler process when created" do
           @cmp.should be_connected
         end
         it "generates an error if there is no compiler process"
@@ -45,7 +47,7 @@ describe Compiler do
       it "reads data from a QPL file when given a filename"
       it "sends QPL code to the compiler process"
       it "receives QPO code from the compiler process"
-      it "writes a .qpo file with the same name as the original .qpl file with the corresponding QPO code" 
+      it "writes a .qpo file with the same name as the original .qpl file with the corresponding QPO code"
     end
   end
 end
--- src/communications/compiler.rb
+++ src/communications/compiler.rb
@@ -2,13 +2,25 @@ require 'socket'
 
 class Compiler
   attr :port
-  
-  def initialize(port=7863)
+
+  def initialize(port=7683)
     @port = port
+
+  end
+
+  def connect
     begin
-      @connection = TCPSocket.new "localhost", @port
-    rescue Errno::ECONNREFUSED => e
-      raise CompilerProcessNotFound, "There was no process found on port #{@port}. Please start 'lpql-compiler'."
+      @connection = TCPSocket.new "::1", @port
+    rescue Errno::ECONNREFUSED => e1
+      begin
+        @connection = TCPSocket.new "localhost", @port
+      rescue Errno::ECONNREFUSED => e
+        raise CompilerProcessNotFound, "There was no process found on port #{@port}. Please start 'lpql-compiler-server'."
+      end
     end
+  end
+
+  def connected?
+    @connection != nil
   end
 end
