1a254f3d2e721b4fcd672f8df42630175686af24
Brett G. Giles <brett.giles@ucalgary.ca>
2014-05-13 19:40:15 -0600

    rubocopped drawing spec and duck matcher spec

--- spec/utility/duck_matcher_spec.rb
+++ spec/utility/duck_matcher_spec.rb
@@ -1,82 +1,85 @@
+# Encoding: UTF-8
 require 'spec/spec_helper'
 
 describe DuckMatcher do
-  it "is created from a start and stop string, followed by a third string" do
-    dm = DuckMatcher.new "start","stop", "entire"
+  it 'is created from a start and stop string,  followed by a third string' do
+    dm = DuckMatcher.new 'start', 'stop', 'entire'
     expect(dm).not_to be_nil
   end
-  describe "match string" do
+  describe 'match string' do
     before :each do
-      @dm = DuckMatcher.new "strt","stop", "entire"
+      @dm = DuckMatcher.new 'strt', 'stop', 'entire'
     end
-    it "should match the start string 'strt'" do
-      expect(@dm.matchss.match("strt")).not_to be_nil
+    it 'should match the start string strt' do
+      expect(@dm.matchss.match('strt')).not_to be_nil
     end
-    it "should match the stop string 'stop'" do
-      expect(@dm.matchss.match("stop")).not_to be_nil
+    it 'should match the stop string stop' do
+      expect(@dm.matchss.match('stop')).not_to be_nil
     end
-    it "should match the entire string 'entire'" do
-      expect(@dm.matchss.match("entire")).not_to be_nil
+    it 'should match the entire string entire' do
+      expect(@dm.matchss.match('entire')).not_to be_nil
     end
   end
-  describe "_matched_paired_start_stop" do
+  describe '_matched_paired_start_stop' do
     before :each do
-      @dm = DuckMatcher.new "strt","stop", "entire"
+      @dm = DuckMatcher.new 'strt', 'stop', 'entire'
       @dm.recurss = 0
     end
-    it "should increment when passed an enumerable a where a[:strt] is not false" do
-      @dm._matched_paired_start_stop?({:strt => true})
+    it 'should increment when passed an enumerable a where a[:strt] is not false' do
+      @dm._matched_paired_start_stop?(strt: true)
       expect(@dm.recurss).to eq(1)
     end
-    it "should decrement when passed an enumerable a where a[:stop] is not false" do
-      @dm._matched_paired_start_stop?({:stop => true})
+    it 'should decrement when passed an enumerable a where a[:stop] is not false' do
+      @dm._matched_paired_start_stop?(stop: true)
       expect(@dm.recurss).to eq(-1)
     end
-    it "should do nothing and return true when passed an enumerable a where a[:strt] and a[:stop] are false" do
-      expect(@dm._matched_paired_start_stop?({}) ).to be true
+    it 'should do nothing and return true when passed an enumerable
+        a where a[:strt] and a[:stop] are false' do
+      expect(@dm._matched_paired_start_stop?({})).to be true
     end
   end
-  describe "match" do
-    before ("each") do
-      @dm = DuckMatcher.new "strt","stop", "entire"
+  describe 'match' do
+    before :each do
+      @dm = DuckMatcher.new 'strt', 'stop', 'entire'
     end
-    it "matches for the third element first and returns that if it exists" do
-      m = @dm.match "entire"
+    it 'matches for the third element first and returns that if it exists' do
+      m = @dm.match 'entire'
       expect(m.length).to eq(1)
-      expect(m[0]).to eq("entire")
+      expect(m[0]).to eq('entire')
     end
-    it "returns nil if the input is empty" do
-      m = @dm.match ""
+    it 'returns nil if the input is empty' do
+      m = @dm.match ''
       expect(m).to be_nil
     end
-    it "returns nil if the input is not matched" do
-      m = @dm.match "strtstrtstrtstop"
+    it 'returns nil if the input is not matched' do
+      m = @dm.match 'strtstrtstrtstop'
       expect(m).to be_nil
     end
-    it "partitions the 'strtxxxstop' into a 1 element array ['strtxxxstop']" do
-      m = @dm.match "strtxxxstop"
+    it 'partitions the strtxxxstop into a 1 element array [strtxxxstop]' do
+      m = @dm.match 'strtxxxstop'
       expect(m.length).to eq(1)
-      expect(m[0]).to eq("strtxxxstop")
+      expect(m[0]).to eq('strtxxxstop')
     end
-    it "partitions the 'strtxxxstopstrtxxxstop' into a 1 element array ['strtxxxstop']" do
-      m = @dm.match "strtxxxstopstrtxxxstop"
+    it 'partitions the strtxxxstopstrtxxxstop into a 1 element array [strtxxxstop]' do
+      m = @dm.match 'strtxxxstopstrtxxxstop'
       expect(m.length).to eq(1)
-      expect(m[0]).to eq("strtxxxstop")
+      expect(m[0]).to eq('strtxxxstop')
     end
-    it "partitions the 'strtxxxstopstrtxxxstopstrtxxxstop' into a 1 element array ['strtxxxstop']" do
-      m = @dm.match "strtxxxstopstrtxxxstopstrtxxxstop"
+    it 'partitions the strtxxxstopstrtxxxstopstrtxxxstop into a 1 element array [strtxxxstop]' do
+      m = @dm.match 'strtxxxstopstrtxxxstopstrtxxxstop'
       expect(m.length).to eq(1)
-      expect(m[0]).to eq("strtxxxstop")
+      expect(m[0]).to eq('strtxxxstop')
     end
-    it "partitions the 'strtxxstrtxxxstopxstop' into a 1 element array ['strtxxstrtxxxstopxstop']" do
-      m = @dm.match "strtxxstrtxxxstopxstop"
+    it 'partitions the strtxxstrtxxxstopxstop into a 1 element array [strtxxstrtxxxstopxstop]' do
+      m = @dm.match 'strtxxstrtxxxstopxstop'
       expect(m.length).to eq(1)
-      expect(m[0]).to eq("strtxxstrtxxxstopxstop")
+      expect(m[0]).to eq('strtxxstrtxxxstopxstop')
     end
-    it "partitions the 'strtxxstrtxstrtxxstrtxxxstopxstopxxstopxstopstrtxxxstop' into a 1 element array ['strtxxstrtxstrtxxstrtxxxstopxstopxxstopxstop']" do
-      m = @dm.match "strtxxstrtxstrtxxstrtxxxstopxstopxxstopxstopstrtxxxstop"
+    it 'partitions the strtxxstrtxstrtxxstrtxxxstopxstopxxstopxstopstrtxxxstop
+        into a 1 element array [strtxxstrtxstrtxxstrtxxxstopxstopxxstopxstop]' do
+      m = @dm.match 'strtxxstrtxstrtxxstrtxxxstopxstopxxstopxstopstrtxxxstop'
       expect(m.length).to eq(1)
-      expect(m[0]).to eq("strtxxstrtxstrtxxstrtxxxstopxstopxxstopxstop")
+      expect(m[0]).to eq('strtxxstrtxstrtxxstrtxxxstopxstopxxstopxstop')
     end
   end
-end      
+end
